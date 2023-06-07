cb3b7070a659413d62ec469168ddbd516f69c38a
vladbv96 <vladbv96@yahoo.com>
2023-06-07 12:51:57 +0300

    -insert documente HR

--- Tickets/HR/HRDocumentGenerator.xaml.cs
+++ Tickets/HR/HRDocumentGenerator.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using iTextSharp.text.pdf;
+using MySql.Data.MySqlClient;
 
 namespace Asgard.Tickets.HR
 {
@@ -23,6 +25,11 @@ namespace Asgard.Tickets.HR
         public HRDocumentGenerator()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
