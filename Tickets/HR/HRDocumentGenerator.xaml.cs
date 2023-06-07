a4aebb1e55bd086902382fd0495c9afaa3d29e6c
vladbv96 <vladbv96@yahoo.com>
2023-06-07 15:21:24 +0300

    -partial gata HR

--- Tickets/HR/HRDocumentGenerator.xaml.cs
+++ Tickets/HR/HRDocumentGenerator.xaml.cs
@@ -1,19 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using iTextSharp.text.pdf;
-using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using Asgard.ViewModels;
 
 namespace Asgard.Tickets.HR
 {
@@ -29,7 +21,102 @@ namespace Asgard.Tickets.HR
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            var user = new MainViewModel();
+            string Nume = user.CurrentUserAccount.Name.ToString();
+            string Prenume = user.CurrentUserAccount.LastName.ToString();
 
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string folderName = nameAngajat.Text; // Replace with your custom folder name
+
+            string templateFolder = "Assets/HR";
+            string templateFileName = "1.Angajamentul consultantului.pdf";
+            string templatePath = Path.Combine(templateFolder, templateFileName);
+
+            string outputFolder = Path.Combine(desktopPath, folderName);
+            Directory.CreateDirectory(outputFolder);
+
+            string newFileName = Path.Combine(outputFolder, $"1.Angajamentul consultantului-{nameAngajat.Text}.pdf");
+            FileStream fileStream = new FileStream(newFileName, FileMode.Create, FileAccess.Write);
+
+            try
+            {
+                PdfReader pdfReader = new PdfReader(templatePath);
+                PdfStamper pdfStamper = new PdfStamper(pdfReader, fileStream);
+                AcroFields acroFields = pdfStamper.AcroFields;
+
+                acroFields.SetField("nume", nameAngajat.Text);
+                acroFields.SetField("prenume", prenumeAngajat.Text);
+
+                pdfStamper.Close();
+                fileStream.Close();
+
+  
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+
+            string templateFolder2 = "Assets/HR";
+            string templateFileName2 = "2.X2 Consimtamant salariati prelucrare imagine_foto_video_final.pdf";
+            string templatePath2 = Path.Combine(templateFolder2, templateFileName2);
+
+            string outputFolder2 = Path.Combine(desktopPath, folderName);
+            Directory.CreateDirectory(outputFolder2);
+
+            string newFileName2 = Path.Combine(outputFolder2, $"2.X2 Consimtamant salariati prelucrare imagine_foto_video_final-{nameAngajat.Text}.pdf");
+            FileStream fileStream2 = new FileStream(newFileName2, FileMode.Create, FileAccess.Write);
+
+            try
+            {
+                PdfReader pdfReader = new PdfReader(templatePath2);
+                PdfStamper pdfStamper = new PdfStamper(pdfReader, fileStream2);
+                AcroFields acroFields = pdfStamper.AcroFields;
+
+                acroFields.SetField("nume", nameAngajat.Text);
+                acroFields.SetField("prenume", prenumeAngajat.Text);
+
+                pdfStamper.Close();
+                fileStream2.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+
+            string templateFolder3 = "Assets/HR";
+            string templateFileName3 = "3.AA GDPR_WFH.pdf";
+            string templatePath3 = Path.Combine(templateFolder3, templateFileName3);
+
+            string outputFolder3 = Path.Combine(desktopPath, folderName);
+            Directory.CreateDirectory(outputFolder3);
+
+            string newFileName3 = Path.Combine(outputFolder2, $"3.AA GDPR_WFH-{nameAngajat.Text}.pdf");
+            FileStream fileStream3 = new FileStream(newFileName3, FileMode.Create, FileAccess.Write);
+
+            try
+            {
+                PdfReader pdfReader = new PdfReader(templatePath3);
+                PdfStamper pdfStamper = new PdfStamper(pdfReader, fileStream3);
+                AcroFields acroFields = pdfStamper.AcroFields;
+
+                acroFields.SetField("nume", nameAngajat.Text);
+                acroFields.SetField("prenume", prenumeAngajat.Text);
+                acroFields.SetField("HR", Nume+" "+Prenume);
+
+                pdfStamper.Close();
+                fileStream3.Close();
+
+                MessageBox.Show("PDF salvat.");
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
         }
     }
 }
