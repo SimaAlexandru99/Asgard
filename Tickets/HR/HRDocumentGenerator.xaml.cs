eb67a3f7a065318d155be6b98db97c8436bad05c
vladbv96 <vladbv96@yahoo.com>
2023-06-07 17:12:34 +0300

    -PDF HR, am finalizat PDF 6

--- Tickets/HR/HRDocumentGenerator.xaml.cs
+++ Tickets/HR/HRDocumentGenerator.xaml.cs
@@ -93,7 +93,7 @@ namespace Asgard.Tickets.HR
             string outputFolder3 = Path.Combine(desktopPath, folderName);
             Directory.CreateDirectory(outputFolder3);
 
-            string newFileName3 = Path.Combine(outputFolder2, $"3.AA GDPR_WFH-{nameAngajat.Text}.pdf");
+            string newFileName3 = Path.Combine(outputFolder3, $"3.AA GDPR_WFH-{nameAngajat.Text}.pdf");
             FileStream fileStream3 = new FileStream(newFileName3, FileMode.Create, FileAccess.Write);
 
             try
@@ -108,6 +108,94 @@ namespace Asgard.Tickets.HR
 
                 pdfStamper.Close();
                 fileStream3.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+
+            string templateFolder4 = "Assets/HR";
+            string templateFileName4 = "4.Anexa Fisa autoevaluare SSM Optima.pdf";
+            string templatePath4 = Path.Combine(templateFolder4, templateFileName4);
+
+            string outputFolder4 = Path.Combine(desktopPath, folderName);
+            Directory.CreateDirectory(outputFolder4);
+
+            string newFileName4 = Path.Combine(outputFolder4, $"4.Anexa Fisa autoevaluare SSM Optima-{nameAngajat.Text}.pdf");
+            FileStream fileStream4 = new FileStream(newFileName4, FileMode.Create, FileAccess.Write);
+
+            try
+            {
+                PdfReader pdfReader = new PdfReader(templatePath4);
+                PdfStamper pdfStamper = new PdfStamper(pdfReader, fileStream4);
+                AcroFields acroFields = pdfStamper.AcroFields;
+
+                acroFields.SetField("nume", nameAngajat.Text);
+                acroFields.SetField("prenume", prenumeAngajat.Text);
+
+                pdfStamper.Close();
+                fileStream4.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+
+            string templateFolder5 = "Assets/HR";
+            string templateFileName5 = "5.Anexa SSM _ WFH.pdf";
+            string templatePath5 = Path.Combine(templateFolder5, templateFileName5);
+
+            string outputFolder5 = Path.Combine(desktopPath, folderName);
+            Directory.CreateDirectory(outputFolder5);
+
+            string newFileName5 = Path.Combine(outputFolder5, $"5.Anexa SSM _ WFH-{nameAngajat.Text}.pdf");
+            FileStream fileStream5 = new FileStream(newFileName5, FileMode.Create, FileAccess.Write);
+
+            try
+            {
+                PdfReader pdfReader = new PdfReader(templatePath5);
+                PdfStamper pdfStamper = new PdfStamper(pdfReader, fileStream5);
+                AcroFields acroFields = pdfStamper.AcroFields;
+
+                acroFields.SetField("nume", nameAngajat.Text);
+                acroFields.SetField("prenume", prenumeAngajat.Text);
+
+                pdfStamper.Close();
+                fileStream5.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+
+            string templateFolder6 = "Assets/HR";
+            string templateFileName6 = "6.Angajament RI 1 ex.pdf";
+            string templatePath6 = Path.Combine(templateFolder6, templateFileName6);
+
+            string outputFolder6 = Path.Combine(desktopPath, folderName);
+            Directory.CreateDirectory(outputFolder6);
+
+            string newFileName6 = Path.Combine(outputFolder6, $"6.Angajament RI 1 ex-{nameAngajat.Text}.pdf");
+            FileStream fileStream6 = new FileStream(newFileName6, FileMode.Create, FileAccess.Write);
+
+            try
+            {
+                PdfReader pdfReader = new PdfReader(templatePath6);
+                PdfStamper pdfStamper = new PdfStamper(pdfReader, fileStream6);
+                AcroFields acroFields = pdfStamper.AcroFields;
+
+                acroFields.SetField("nume", nameAngajat.Text);
+                acroFields.SetField("prenume", prenumeAngajat.Text);
+                acroFields.SetField("serieCI", serieCI.Text);
+                acroFields.SetField("numarCI", NumarCI.Text);
+                acroFields.SetField("dataT", dataAngajare.Text);
+
+                pdfStamper.Close();
+                fileStream6.Close();
 
                 MessageBox.Show("PDF salvat.");
 
