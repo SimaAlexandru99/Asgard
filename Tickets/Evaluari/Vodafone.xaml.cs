510e2f4aee4a9a6b3561939f2174d2814f66b6dc
vladbv96 <vladbv96@yahoo.com>
2023-06-27 14:57:56 +0300

    -e aproape gata evaluarea pe VDF

---
+++ Tickets/Evaluari/Vodafone.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Asgard.Tickets.Evaluari
+{
+    /// <summary>
+    /// Interaction logic for Vodafone.xaml
+    /// </summary>
+    public partial class Vodafone : Page
+    {
+        public Vodafone()
+        {
+            InitializeComponent();
+        }
+
+        private void TrimiteEvaluarea_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
