9a688e65cd91bb31c08926c2bcf457a25fc856bd
vladbv96 <vladbv96@yahoo.com>
2023-06-28 13:23:34 +0300

    -ham inceput oroc 2023

---
+++ Tickets/Evaluari/OROC2023.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Asgard.Tickets.Evaluari
+{
+    /// <summary>
+    /// Interaction logic for OROC2023.xaml
+    /// </summary>
+    public partial class OROC2023 : Page
+    {
+        public OROC2023()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboProiect_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
