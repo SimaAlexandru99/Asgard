9e04300bd875543beefabd09d18e5e1e30b96480
vladbv96 <vladbv96@yahoo.com>
2023-06-28 14:57:04 +0300

    -INCA CEVA LA OROC 2023

--- Tickets/Evaluari/OROC2023.xaml.cs
+++ Tickets/Evaluari/OROC2023.xaml.cs
@@ -29,14 +29,54 @@ namespace Asgard.Tickets.Evaluari
             emailSuperior.Text = user.CurrentUserAccount.Email;
         }
 
-        private void ComboProiect_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        private void Clear()
         {
-            e.Handled = true;
+            emailAngajat.Text = string.Empty;
+            emailSuperior.Text = string.Empty;
+            numarClient.Text = string.Empty;
+            clientCuFibra.Text = string.Empty;
+            produsulDePeCerere.Text = string.Empty;
+            respectaProcesulDeRetentie.Text = string.Empty;
+            informatiileMandatorii.Text = string.Empty;
+            produseRetentie.Text = string.Empty;
+            ComboProiect.Text = string.Empty;
+            dataApel.Text = string.Empty;
+            ComboTipApel.Text = string.Empty;
+            ComboMotivApel.Text = string.Empty;
+            ComboMotivDesfintare.Text = string.Empty;
+            notaIntrebare1.Text = string.Empty;
+            observatiiIntrebare1.Text = string.Empty;
+            notaIntrebare2.Text = string.Empty;
+            observatiiIntrebare2.Text = string.Empty;
+            notaIntrebare3.Text = string.Empty;
+            observatiiIntrebare3.Text = string.Empty;
+            notaIntrebare4.Text = string.Empty;
+            observatiiIntrebare4.Text= string.Empty;
+            notaIntrebare5.Text = string.Empty;
+            observatiiIntrebare5.Text = string.Empty;
+            notaIntrebare6.Text = string.Empty;
+            observatiiIntrebare6.Text = string.Empty;
+            notaIntrebare7.Text = string.Empty;
+            observatiiIntrebare7.Text= string.Empty;
+            notaIntrebare8.Text = string.Empty;
+            observatiiIntrebare8.Text= string.Empty;
+            notaIntrebare9.Text = string.Empty;
+            observatiiIntrebare9.Text= string.Empty;
+            notaIntrebare10.Text = string.Empty;
+            observatiiIntrebare10.Text= string.Empty;
+            notaIntrebare11.Text = string.Empty;
+            observatiiIntrebare11.Text= string.Empty;
+            observatiiIntrebare12.Text= string.Empty;
         }
+        
 
         private void TrimiteEvaluarea_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+        private void ComboProiect_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            e.Handled = true;
         }
     }
 }
